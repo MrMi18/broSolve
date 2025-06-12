66ccbb51b2d699bee33cd90ff1162c6c366e18e7
MrMi18 <is8388508@gmail.com>
2025-06-12 16:51:46 +0530

    firbase config now private + proper app layout created

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,14 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCpvnIP1EBRiC9RLFF_CYs-OUZh-kJG8eQ",
-  authDomain: "brosolve-d2bee.firebaseapp.com",
-  projectId: "brosolve-d2bee",
-  storageBucket: "brosolve-d2bee.firebasestorage.app",
-  messagingSenderId: "433489591023",
-  appId: "1:433489591023:web:709d84a8a416b3ba49791a",
-  measurementId: "G-Q7QPYQSKY1"
+  apiKey: process.env.API_KEY, 
+  authDomain: process.env.AUTH_DOMAIN, 
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET, 
+  messagingSenderId: process.env.MESSAGING_SENDER_ID, 
+  appId: process.env.APP_ID, 
+  measurementId: process.env.MEASUREMENT_ID 
 };
+
 
 
 const app = initializeApp(firebaseConfig);
